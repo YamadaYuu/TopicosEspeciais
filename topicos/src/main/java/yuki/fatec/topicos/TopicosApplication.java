532ea98480b69796bbe53a394ec523c6c8b30f88
Yamada Anna <66070734+YamadaYuu@users.noreply.github.com>
2022-08-22 22:01:18 -0300

    spring initializr

---
+++ topicos/src/main/java/yuki/fatec/topicos/TopicosApplication.java
@@ -0,0 +1,13 @@
+package yuki.fatec.topicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicosApplication.class, args);
+	}
+
+}
---
+++ topicos/src/test/java/yuki/fatec/topicos/TopicosApplicationTests.java
@@ -0,0 +1,13 @@
+package yuki.fatec.topicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
