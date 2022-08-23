1ffedf071736ce2b89ea4ac215f2699d5d112681
Yamada Anna <66070734+YamadaYuu@users.noreply.github.com>
2022-08-23 19:08:07 -0300

    ddl

---
+++ topicos/src/main/java/yuki/fatec/topicos/repository/userRepository.java
@@ -0,0 +1,7 @@
+package yuki.fatec.topicos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface userRepository extends JpaRepository<Usuario, Long> {
+    
+}
